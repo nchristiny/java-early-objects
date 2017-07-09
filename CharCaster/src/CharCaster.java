8cde862dd022a3e53e6660a44dede20410eb1115
nchristiny <icarus747@gmail.com>
2017-07-09 14:01:58 -0500

    Add CharCaster

---
+++ CharCaster/src/CharCaster.java
@@ -0,0 +1,49 @@
+/**
+ * To cast Char type to Int
+ */
+
+/**
+ * @author Nick Christiny
+ *
+ */
+public class CharCaster {
+
+	/**
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		
+		char chr1 = 'A';
+		char chr2 = 'B';
+		char chr3 = 'C';
+		char chr4 = 'a';
+		char chr5 = 'b';
+		char chr6 = 'c';
+		char chr7 = '0';
+		char chr8 = '1';
+		char chr9 = '2';
+		char chr10 = '$';
+		char chr11 = '*';
+		char chr12 = '+';
+		char chr13 = '/';
+		char chr14 = ' ';
+		
+		System.out.printf("Char: %c to int is %d.%n", chr1, (int) chr1);
+		System.out.printf("Char: %c to int is %d.%n", chr2, (int) chr2);
+		System.out.printf("Char: %c to int is %d.%n", chr3, (int) chr3);
+		System.out.printf("Char: %c to int is %d.%n", chr4, (int) chr4);
+		System.out.printf("Char: %c to int is %d.%n", chr5, (int) chr5);
+		System.out.printf("Char: %c to int is %d.%n", chr6, (int) chr6);
+		System.out.printf("Char: %c to int is %d.%n", chr7, (int) chr7);
+		System.out.printf("Char: %c to int is %d.%n", chr8, (int) chr8);
+		System.out.printf("Char: %c to int is %d.%n", chr9, (int) chr9);
+		System.out.printf("Char: %c to int is %d.%n", chr10, (int) chr10);
+		System.out.printf("Char: %c to int is %d.%n", chr11, (int) chr11);
+		System.out.printf("Char: %c to int is %d.%n", chr12, (int) chr12);
+		System.out.printf("Char: %c to int is %d.%n", chr13, (int) chr13);
+		System.out.printf("Char: %c (whitespace) to int is %d.%n", chr14, (int) chr14);
+
+	}
+
+}
