91594a6941dc48c6d1e301707253993969d6876d
nchristiny <icarus747@gmail.com>
2017-07-12 10:56:43 -0500

    Add exit button b2

--- SimpleGUI/src/SimpleGUI.java
+++ SimpleGUI/src/SimpleGUI.java
@@ -9,11 +9,13 @@ import javax.swing.*;
  * @author Nick Christiny
  *
  */
+
 public class SimpleGUI extends JFrame implements ActionListener
 {
 	private static final long serialVersionUID = 1L;
 	JLabel l1, l2, l3;
-	JButton b1; 
+	// Add button b2
+	JButton b1, b2; 
 	JTextField t1, t2, t3;
 
 	SimpleGUI() 
@@ -22,6 +24,10 @@ public class SimpleGUI extends JFrame implements ActionListener
 		l2 = new JLabel(" INPUT 2");
 		l3 = new JLabel(" OUTPUT");
 		b1 = new JButton("BUTTON 1");
+		
+		// Instantiate button b2 label
+		b2 = new JButton("EXIT");
+		
 		t1 = new JTextField(10);
 		t2 = new JTextField(10);
 		t3 = new JTextField(10);
@@ -35,6 +41,12 @@ public class SimpleGUI extends JFrame implements ActionListener
 		add(b1);
 		b1.addActionListener(this);
 
+		// Add new instance of button b2
+		add(b2);
+		
+		// Add button b2 event listener and action
+		b2.addActionListener(e -> System.exit(0));
+		
 		setSize(500,300);
 		setLayout(new GridLayout(4,2));
 		setTitle("Simple Java GUI");
