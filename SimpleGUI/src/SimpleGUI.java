0def191de5491eb3925d9b9a1c4f16468eac4fc9
nchristiny <icarus747@gmail.com>
2017-07-12 11:14:45 -0500

    Add bonus pay checkbox

--- SimpleGUI/src/SimpleGUI.java
+++ SimpleGUI/src/SimpleGUI.java
@@ -13,21 +13,28 @@ import javax.swing.*;
 public class SimpleGUI extends JFrame implements ActionListener
 {
 	private static final long serialVersionUID = 1L;
-	JLabel l1, l2, l3;
+	// Add label l4
+	JLabel l1, l2, l3, l4;
 	// Add button b2
 	JButton b1, b2; 
 	JTextField t1, t2, t3;
+	// Add checkbox
+	JCheckBox check1;
 
 	SimpleGUI() 
 	{
-		l1 = new JLabel(" INPUT 1");
-		l2 = new JLabel(" INPUT 2");
-		l3 = new JLabel(" OUTPUT");
-		b1 = new JButton("BUTTON 1");
+		l1 = new JLabel(" Current Salary");
+		l2 = new JLabel(" Percent Rate");
+		l3 = new JLabel(" New Salary");
+		b1 = new JButton("COMPUTE");
 		
-		// Instantiate button b2 label
+		// Instantiate button b2 and label
 		b2 = new JButton("EXIT");
-		
+		// Instantiate check1 checkbox l4 and label
+		l4 = new JLabel("  ");
+		check1 = new JCheckBox("bonus pay");
+		check1.setSelected(true);
+
 		t1 = new JTextField(10);
 		t2 = new JTextField(10);
 		t3 = new JTextField(10);
@@ -38,6 +45,11 @@ public class SimpleGUI extends JFrame implements ActionListener
 		add(t2);
 		add(l3);
 		add(t3);
+		
+		// Insert checkbox
+		add(check1);
+		add(l4);
+
 		add(b1);
 		b1.addActionListener(this);
 
@@ -48,7 +60,10 @@ public class SimpleGUI extends JFrame implements ActionListener
 		b2.addActionListener(e -> System.exit(0));
 		
 		setSize(500,300);
-		setLayout(new GridLayout(4,2));
+		
+		// Add row for checkbox
+		setLayout(new GridLayout(5,2));
+		
 		setTitle("Simple Java GUI");
 	}
 
@@ -59,7 +74,17 @@ public class SimpleGUI extends JFrame implements ActionListener
 		{
 			a = Float.parseFloat(t1.getText());
 			b = Float.parseFloat(t2.getText());
-			c = a + b;
+			// Add percentage calculation 
+			c = a + (a * b / 100);
+			if (check1.isSelected()) 
+			{
+				// Add bonus pay
+				c += 500;
+			} 
+			else 
+			{
+				// Do nothing  - can eliminate 
+			}
 			t3.setText(String.valueOf(c));
 		} 
 	}
